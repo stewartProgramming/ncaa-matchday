9d3b0dc3f2f86a1cdfccf7d46bca4fedc6e0b9d2
Stewart <stewartprogramming@gmail.com>
2024-08-28 13:24:35 -0400

    initial setup

---
+++ ncaa-matchday/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ncaa_matchday.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
